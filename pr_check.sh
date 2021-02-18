9adf497eb44dead19c2e1b176fb14111a42dbafa
bholifie <bholifie@redhat.com>
2021-02-18 15:05:03 -0600

    split out unit tests to after building source

--- pr_check.sh
+++ pr_check.sh
@@ -23,91 +23,6 @@ if ! (pre-commit run --all-files); then
 fi
 
 # --------------------------------------------
-# Unit testing Django
-# --------------------------------------------
-
-function killbg {
-  echo "Caught signal, kill port forward"
-  kill $BG_PID
-}
-
-function nsrelease {
-  echo "Release bonfire namespace"
-  bonfire namespace release $NAMESPACE
-}
-
-function random_unused_port { local port=$(shuf -i 2000-65000 -n 1)
-    netstat -lat | grep $port > /dev/null
-    if [[ $? == 1 ]] ; then
-        RANDOM_PORT=$port
-    else
-        random_unused_port
-    fi
-}
-
-#
-# Install Bonfire
-#
-if ! (which bonfire >/dev/null); then
-    git clone https://github.com/RedHatInsights/bonfire.git
-    pip install --upgrade pip setuptools wheel pipenv tox psycopg2-binary
-    pip install ./bonfire
-fi
-
-#
-# Deploy ClowdApp to get DB instance
-#
-
-NAMESPACE=$(bonfire namespace reserve)
-oc project $NAMESPACE
-
-cat << EOF > config.yaml
-envName: env-$NAMESPACE
-apps:
-- name: host-inventory
-  host: local
-  repo: $PWD
-  path: deployment.yaml
-  parameters:
-    IMAGE: $IMAGE
-EOF
-
-bonfire local get -a ${APP_NAME} | oc apply -f -
-bonfire namespace wait-on-resources $NAMESPACE
-
-#
-# Grab DB creds
-#
-
-# Get a random port on the jenkins agent to forward
-random_unused_port
-
-oc get secret ${APP_NAME} -o json | jq -r '.data["cdappconfig.json"]' | base64 -d | jq .database > db-creds.json
-
-export INVENTORY_DB_NAME=$(jq -r .name < db-creds.json)
-export INVENTORY_DB_HOST=localhost
-export INVENTORY_DB_PORT=$RANDOM_PORT
-export INVENTORY_DB_USER=$(jq -r .adminUsername < db-creds.json)
-export INVENTORY_DB_PASS=$(jq -r .adminPassword < db-creds.json)
-export PGPASSWORD=$(jq -r .adminPassword < db-creds.json)
-
-oc port-forward svc/${APP_NAME}-db $RANDOM_PORT:5432 &
-BG_PID=$!
-trap killbg EXIT SIGINT SIGKILL TERM
-trap nsrelease SIGINT SIGKILL TERM
-
-python manage.py db upgrade
-pytest --cov=. --junitxml=junit.xml --cov-report html -sv
-
-mkdir -p $WORKSPACE/artifacts
-cat << EOF > ${WORKSPACE}/artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
-nsrelease
-
-# --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
 COMPONENT_NAME="host-inventory"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
@@ -120,10 +35,18 @@ IQE_FILTER_EXPRESSION=""
 # We'll take it from here ...
 # ---------------------------
 
+# Get bonfire helper scripts
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh -o bootstrap.sh
 source bootstrap.sh  # checks out bonfire and changes to "cicd" dir...
+
+# build the PR commit image
 source build.sh
+
+# Run the django unit tests
+source ${WORKSPACE}/unit_test.sh
+
+# Smoke test the App (iqe tests coming soon)
 source deploy_ephemeral_env.sh
 # source smoke_test.sh
 
